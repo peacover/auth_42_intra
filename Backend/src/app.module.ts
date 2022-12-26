d266e68462b414d8c57fa73661bf4e02873e341a
Peacover <yer-raki@student.1337.ma>
2022-12-26 17:02:01 +0100

    merged!

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -3,12 +3,15 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { GameModule } from './game/app.module';
 
 @Module({
   imports: [
     AuthModule,
     UsersModule,
+    GameModule,
     PrismaModule,
+
     ConfigModule.forRoot({
       isGlobal: true,
     })
